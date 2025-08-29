473abd113dbf743d5a51fcb8d2dd84f1c3f9d198
kartikpashte <kartikpashte58@gmail.com>
2025-08-29 13:38:47 +0000

    upgrade

--- first-app/src/App.jsx
+++ first-app/src/App.jsx
@@ -1,21 +1,40 @@
-import Home from "./Home.jsx";
-import About from "./About.jsx";
-import Contact from "./Contact.jsx";
+// import Home from "./Home.jsx";
+// import About from "./About.jsx";
+// import Contact from "./Contact.jsx";
 
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
+import './style.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <Home/>
-      <About/>
-      <Contact/>
+    <body>
+    <nav class="navbar">
+        <div class="logo">
+            <img src={viteLogo}
+                alt="Logo"></img>
+        </div>
 
+        <form class="search-box">
+            <input type="text" name="query" placeholder="Search..."></input>
+            <button type="submit">🔍</button>
+        </form>
+
+        <div class="links">
+            <ul>
+                <li><a href="">Home</a></li>
+                <li><a href="">Projects</a></li>
+                <li><a href="">About</a></li>
+                <li><a href="">Contact</a></li>
+            </ul>
+        </div>
+    </nav>
+
+</body>
     </>
   )
 }

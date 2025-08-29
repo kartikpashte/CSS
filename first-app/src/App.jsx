0fe009d4ce22f0222369ef625df41bbf32ad3da3
kartikpashte <kartikpashte58@gmail.com>
2025-08-29 12:47:18 +0000

    new file

---
+++ first-app/src/App.jsx
@@ -0,0 +1,23 @@
+import Home from "./Home.jsx";
+import About from "./About.jsx";
+import Contact from "./Contact.jsx";
+
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <Home/>
+      <About/>
+      <Contact/>
+
+    </>
+  )
+}
+
+export default App

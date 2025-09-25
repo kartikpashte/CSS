1f9990499640dfe46b91aa0768ea327ba49062d4
kartikpashte <kartikpashte58@gmail.com>
2025-09-25 13:17:01 +0000

    new file

--- first-app/src/App.jsx
+++ first-app/src/App.jsx
@@ -7,44 +7,55 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import Movie from "./Movie"
 import './style.css'
+import Form from './topic/Form'
+
+// function App() {
+//   const [count, setCount] = useState(0)
+
+//   return (
+//     <>
+//     <body>
+//     <nav class="navbar">
+//         <div class="logo">
+//             <img src={viteLogo}
+//                 alt="Logo"></img>
+//         </div>
+
+//         <form class="search-box">
+//             <input type="text" name="query" placeholder="Search..."></input>
+//             <button type="submit">🔍</button>
+//         </form>
+
+//         <div class="links">
+//             <ul>
+//                 <li><a href="">Home</a></li>
+//                 <li><a href="">Projects</a></li>
+//                 <li><a href="">About</a></li>
+//                 <li><a href="">Contact</a></li>
+//             </ul>
+//         </div>
+//     </nav>
+
+//     <div className="main">
+//         <h1>Welcome to Movie App</h1>
+//         <div className="wrapper">
+//         <Movie />
+//       </div>
+//         {/* <div className="card"><img src={viteLogo}alt="Logo"></img></div> */}
+
+//     </div>
+
+// </body>
+
+//     </>
+//   )
+// }
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-    <body>
-    <nav class="navbar">
-        <div class="logo">
-            <img src={viteLogo}
-                alt="Logo"></img>
-        </div>
-
-        <form class="search-box">
-            <input type="text" name="query" placeholder="Search..."></input>
-            <button type="submit">🔍</button>
-        </form>
-
-        <div class="links">
-            <ul>
-                <li><a href="">Home</a></li>
-                <li><a href="">Projects</a></li>
-                <li><a href="">About</a></li>
-                <li><a href="">Contact</a></li>
-            </ul>
-        </div>
-    </nav>
-
-    <div className="main">
-        <h1>Welcome to Movie App</h1>
-        <div className="wrapper">
-        <Movie />
-      </div>
-        {/* <div className="card"><img src={viteLogo}alt="Logo"></img></div> */}
-
-    </div>
-
-</body>
+    <Form/>
     </>
   )
 }
--- first-app/src/topic/Form.jsx
+++ first-app/src/topic/Form.jsx
@@ -3,11 +3,19 @@ import { useState } from 'react'
 const Form = () => {
 
     const [name,setName] = useState("");
+    const [letter,setLetter] = useState(0);
+
     const change = (e) => {
         e.preventDefault();
         setName(e.target.value);
 
     }
+
+  const count = (e) => {
+        e.preventDefault();
+
+    setLetter(name.length);
+  }
 
   return (
     <div>
@@ -16,8 +24,8 @@ const Form = () => {
             <input onChange={change} value={name} type="text" id='uname' placeholder='Enter Name' />
             <br />
 
-            <input onClick={change} type="submit" value={name} />
-            <p>number of letters writen is {name.length}</p>
+            <input onClick={count} type="submit" value={"count"} />
+            <p>number of letters writen is {letter}</p>
         </form>
     </div>
   )

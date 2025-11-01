958b56059db89204c30cfc87cd0d857b24b4be3f
kartikpashte <kartikpashte58@gmail.com>
2025-11-01 13:26:48 +0000

    changes

--- Projects/movie-app/src/App.jsx
+++ Projects/movie-app/src/App.jsx
@@ -6,35 +6,36 @@ import './style.css'
 function App() {
   return (
     <>
-    <div>
-      <nav className="navbar">
-        <div className="logo">
-          <img src={viteLogo} alt="Logo" />
-        </div>
+      <div>
+        <nav className="navbar">
+          <div className="logo">
+            <img src={viteLogo} alt="Logo" />
+          </div>
 
-        <form className="search-box">
-          <input type="text" name="query" placeholder="Search..." />
-          <button type="submit">🔍</button>
-        </form>
+          <form className="search-box">
+            <input type="text" name="query" placeholder="Search..." />
+            <button type="submit">🔍</button>
+          </form>
 
-        <div className="links">
-          <ul>
-            <li><a href="">Home</a></li>
-            <li><a href="">Projects</a></li>
-            <li><a href="">About</a></li>
-            <li><a href="">Contact</a></li>
-          </ul>
-        </div>
-      </nav>
+          <div className="links">
+            <ul>
+              <li><a href="">Home</a></li>
+              <li><a href="">Projects</a></li>
+              <li><a href="">About</a></li>
+              <li><a href="">Contact</a></li>
+            </ul>
+          </div>
+        </nav>
 
-      <main className="main">
-        <h1>Welcome to Movie App</h1>
-        <div className="wrapper">
-          <Movie />
+        <main className="main">
+          <h1>Welcome to Movie App</h1>
+          <div className="wrapper">
+            <Movie />
 
-        </div>
-      </main>
-    </div>
+
+          </div>
+        </main>
+      </div>
     </>
   )
 }

73ca52e4554dc7a85af25f1cbacabd1140952748
kartikpashte <kartikpashte58@gmail.com>
2025-12-19 13:20:54 +0000

    new file

---
+++ Tailwind/app/routes.ts
@@ -0,0 +1,8 @@
+import { type RouteConfig, index,route, layout,prefix,} from "@react-router/dev/routes";
+
+
+export default [
+    index("routes/home.tsx"),
+    route("about", "./routes/about.tsx"),
+    route("contact", "./routes/contact.tsx"),
+] satisfies RouteConfig;
---
+++ Tailwind/app/routes/home.tsx
@@ -0,0 +1,21 @@
+import type { Route } from "./+types/home";
+// import { Welcome } from "../welcome/welcome";
+import Navbar from "~/components/Navbar";
+import Hero from "~/components/Hero";
+
+export function meta({ }: Route.MetaArgs) {
+  return [
+    { title: "New React Router App" },
+    { name: "description", content: "Welcome to React Router!" },
+  ];
+}
+
+export default function Home() {
+  return (
+  <>
+    <Navbar />
+    <Hero />
+    
+  </>
+  );
+}
